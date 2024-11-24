c6cf6ba594518937f5a362c57ed8f120fd3f8697
Lakshmi Srinivas <lakshmisrinivasgeo@gmail.com>
2024-11-24 20:19:56 +0530

    Edited in new pipeline

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
 
-    <h1>MLOps App Edited everything now</h1>
+    <h1>MLOps App Edited-Pipeline Check</h1>
 	<h2>Upload your data to get Gen Ai predictions</h2>
     <img
       alt="Vue logo"
